099f3de5de0e4b5d20f635d3f1b75c1f62770512
kevinckc <kevinckc918@gmail.com>
2026-07-02 11:09:52 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 if [ -n "$FIREBASE_CONFIG" ]; then
   echo "發現 FIREBASE_CONFIG 環境變數，正在寫入設定檔..."
   echo "$FIREBASE_CONFIG" > firebase-config.json
-  echo "✅ firebase-config.json 寫入成功！"
+  echo "firebase-config.json 寫入成功！"
 else
-  echo "⚠️ 警告: 未發現 FIREBASE_CONFIG 環境變數，系統將繼續使用單機模式或手動粘貼設定。"
+  echo "警告: 未發現 FIREBASE_CONFIG 環境變數，系統將繼續使用單機模式或手動粘貼設定。"
 fi
